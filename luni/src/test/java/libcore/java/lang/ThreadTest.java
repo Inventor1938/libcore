25f7fb691eb599f381f2f13f3d23a0fd6e0278d9
Neil Fuller <nfuller@google.com>
2016-06-29 15:02:52 +0100

    Remove an infinite loop / put a Thread out of its misery
    ThreadTest.testGetStackTrace() created a thread that never
    terminated due to a for loop with a missing increment.
    
    The loop was unnecessary and has been removed.
    
    Change-Id: If00018b6330fe1340c293585fb9d64206dd89e20
    Test: Ran the CTS tests
    Bug: 29820565
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- luni/src/test/java/libcore/java/lang/ThreadTest.java
+++ luni/src/test/java/libcore/java/lang/ThreadTest.java
@@ -134,11 +134,9 @@ public final class ThreadTest extends TestCase {
                 doSomething();
             }
             public void doSomething() {
-                for (int i = 0; i < 20;) {
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException ignored) {
-                    }
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException ignored) {
                 }
             }
         };
@@ -150,6 +148,7 @@ public final class ThreadTest extends TestCase {
         // Expect to find MyThread.doSomething in the trace
         assertTrue(trace.getClassName().contains("ThreadTest")
                 && trace.getMethodName().equals("doSomething"));
+        t1.join();
     }
 
     public void testGetAllStackTracesIncludesAllGroups() throws Exception {
